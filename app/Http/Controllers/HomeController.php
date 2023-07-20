905de423adebc6b25cee2976ab2f1416d3c3948c
Your Name <you@example.com>
2023-07-20 16:10:29 +0500

    Updated home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,22 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Determine the user's role (assuming you have the 'role' field in the users table)
+        $userRole = auth()->user()->role;
+
+        // Redirect the user to the appropriate dashboard based on their role
+        switch ($userRole) {
+            case 'admin':
+                return view('home_admin');
+                break;
+            case 'employer':
+                return view('home_employer');
+                break;
+            case 'labour':
+                return view('home_labour');
+                break;
+            default:
+                return view('home_default'); // A default view in case the user's role doesn't match any of the above.
+        }
     }
 }
