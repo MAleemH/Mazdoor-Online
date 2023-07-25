9515c13b59ee6bfad739b7f5dceb7a58a257ee6a
Your Name <you@example.com>
2023-07-25 16:32:47 +0500

    Updated portfolio controller

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -4,25 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Portfolio;
+use App\Models\WorkCategory;
 use Illuminate\Support\Facades\Auth;
 
 class PortfolioController extends Controller
 {
     public function index()
     {
-        $portfolios = Portfolio::where('user_id', Auth::id())->get();
-        return view('portfolios.index', compact('portfolios'));
+        $user = auth()->user();
+        $portfolios = Portfolio::where('user_id', $user->id)->get();
+        return view('portfolios.index', compact('portfolios', 'user'));
     }
 
     public function create()
     {
-        return view('portfolios.create');
+        $specializations = WorkCategory::all();
+        return view('portfolios.create', compact('specializations'));
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'specialization' => 'required|string|max:255',
+            'specialization' => 'required',
             'description' => 'required|string',
         ]);
 
@@ -43,7 +46,10 @@ class PortfolioController extends Controller
             return redirect()->route('portfolios.index')->with('error', 'You are not authorized to edit this portfolio.');
         }
 
-        return view('portfolios.edit', compact('portfolio'));
+        // Fetch all work categories
+        $workCategories = WorkCategory::all();
+
+        return view('portfolios.edit', compact('portfolio', 'workCategories'));
     }
 
     public function update(Request $request, Portfolio $portfolio)
