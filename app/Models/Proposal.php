5828c2897403178dd739821d5c16aa51bc5afa67
Your Name <you@example.com>
2023-07-26 14:13:21 +0500

    Added proposal model

---
+++ app/Models/Proposal.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Proposal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'job_id',
+        'proposal_text',
+        'price', // Add 'price' to the fillable array
+    ];
+
+    // Define relationships (if any)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+}
