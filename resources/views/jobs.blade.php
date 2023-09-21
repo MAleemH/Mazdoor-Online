6fbfd6f993bed5747e016789f64f300e6e8d5367
Your Name <you@example.com>
2023-09-21 08:56:30 +0500

    Created jobs page

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-12 border-bottom mt-3">
+            <h4 class="page-title">Jobs</h4>
+        </div>
+        <div class="col-12 mt-4">
+            <p>
+                Online Mazdoor is a worker welfare system designed to transform how a person finds work. We bridge the gap between an employer and a worker by providing an easy to use online system for searching and hiring everyday workers such as drivers, maids, electricians, and plumbers, among a host of similar blue-collar / non-formal professions.
+            </p>
+        </div>
+        <div class="col-12 mt-4 d-flex justify-content-around">
+            <div class="card text-center about-cards">
+              <div class="card-body">
+                <h4 class="card-title">Users</h4>
+                <p class="card-text">70,000+</p>
+              </div>
+            </div>
+            <div class="card text-center about-cards">
+              <div class="card-body">
+                <h4 class="card-title">Workers</h4>
+                <p class="card-text">75,000+</p>
+              </div>
+            </div>
+            <div class="card text-center about-cards">
+              <div class="card-body">
+                <h4 class="card-title">Skills</h4>
+                <p class="card-text">70+</p>
+              </div>
+            </div>
+            <div class="card text-center about-cards">
+              <div class="card-body">
+                <h4 class="card-title">Citites</h4>
+                <p class="card-text">10+</p>
+              </div>
+            </div>
+        </div>
+        <div class="col-12 mt-4">
+            <p>
+                Online Mazdoor also has a strong social aspect as it plans to change the lives of millions of workers who are not technology literate. We help them reap the benefits of social media without the need of knowhow or access to technology.
+            </p>
+            <p>
+                At Mazdoor Online, we are dedicated to revolutionizing the labor industry by connecting skilled workers with opportunities that match their expertise and aspirations. Our platform serves as a bridge between employers and talented laborers, facilitating seamless collaboration and empowering individuals to find meaningful work or hire the right talent. We believe in the transformative power of technology to streamline the job-seeking process, making it easier for both employers and laborers to navigate the dynamic labor market. With a commitment to efficiency, transparency, and fairness, Mazdoor Online is not just a platform; it's a catalyst for a brighter future in the labor industry. Join us on this journey as we reshape the way labor works online.
+            </p>
+            <p>
+                At here, we're passionate about redefining the labor landscape. We understand the importance of skilled labor in driving economies and building communities. That's why we've created a platform that empowers laborers, businesses, and industries alike. Our mission is to provide a centralized hub where laborers can showcase their skills, gain exposure to a wide range of opportunities, and find work that aligns with their abilities and interests. Simultaneously, we offer businesses a convenient way to connect with the right talent, ensuring projects are completed efficiently and effectively. Mazdoor Online is committed to fostering a supportive environment that values the contributions of laborers and the growth of businesses. We're more than a platform; we're a catalyst for growth, progress, and empowerment in the world of labor.
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
